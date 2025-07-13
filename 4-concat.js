d95c2c7298c313a9056570aafba8ec9629b019a1
Ivan Kiweewa Mulinda <ivankm2000@gmail.com>
2025-07-13 15:22:55 -0400

    Modify file 4

--- 4-concat.js
+++ 4-concat.js
@@ -1,4 +1,14 @@
-const Arg1 = 'Ivan';
-const Arg2 = 'Hardworking';
+const Arg1 = 'Python';
+const Arg2 = 'fun';
 
 console.log(`${Arg1} is ${Arg2}`);
+
+const Arg3 = 'HBTN';
+const Arg4 = 'undefined';
+
+console.log(`${Arg3} is ${Arg4}`);
+
+const Arg5 = 'undefined';
+const Arg6 = 'undefined';
+
+console.log(`${Arg5} is ${Arg6}`);
