b444a2bab297ac81af96fa1700e7a869e5aa6418
Ivan Kiweewa Mulinda <ivankm2000@gmail.com>
2025-07-14 14:09:45 -0400

    Add file 5

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,13 +1,7 @@
-#!/usr/bin/node
-const num = process.argv[2];
-if (num === undefined){
-  console.log('Not a number');
-} else {
-    const number = parseInt(num, 10);
-}
+const arg = Number(process.argv[2]);
 
-if (isNaN(number)) {
-    console.log('Not a number');
+if (!isNaN(arg)) {
+  console.log('My number: ' + parseInt(arg));
 } else {
-    console.log('My number: ' + number);
+  console.log('Not a number');
 }
